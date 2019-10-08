c91f04c8744ac6489ac5fd1bfd0bd32bb83ec68b
marsnebulasoup <52974834+marsnebulasoup@users.noreply.github.com>
2019-10-08 16:37:32 +0530

    Pushing to test on mobile

--- time/script.js
+++ time/script.js
@@ -2,3 +2,8 @@ function ChangeTimeZone(){
 	var menu = document.getElementById("selTimezone");
 	menu.setAttribute("class", "isActive");
 }
+
+function CloseSelectTimezone(){
+	var menu = document.getElementById("selTimezone");
+	menu.removeAttribute("class", "isActive");
+}
