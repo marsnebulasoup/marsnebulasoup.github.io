1d3a423f63e0a67b78731fd60d1664cb147ad698
marsnebulasoup <52974834+marsnebulasoup@users.noreply.github.com>
2019-11-08 17:17:23 +0530

    Some UI fixes
    Fixed navbar issue
    Fixed space between year and imdb rating.

--- movies/js/parse_movies.js
+++ movies/js/parse_movies.js
@@ -122,7 +122,7 @@ async function Display(results){
 	// var searchbar = document.getElementById("real_searchbar");
 	
 	var middot = ' &middot; ';	
-	bigmovie.navbar = results.length + " results found."
+	navbar.navbar = results.length + " results found."
 	
 	fetch('db/ind/' + results[0].imdbID + ".json")
 	.then(res => {
@@ -187,7 +187,7 @@ async function Display(results){
 		genres.length = 2;
 		genres.toString();
 		
-		var childMovie = '<div id="childMovie" class="tile is-child is-2"><div class="smallmovie"><ul><div class="card"><li><div style="height:17em"><img class="poster-little" src="' + imgurl + '" /></div></li><li><p class="caption">' + title + '<br><span class="movieinfo"><span class="boxed">' + agerating + '</span> &middot; ' + released + ' &middot;' + imdbrating + '</span><br><span class="movieinfo"><span>' + genres + '</span></span></p></li></div></ul></div></div>';
+		var childMovie = '<div id="childMovie" class="tile is-child is-2"><div class="smallmovie"><ul><div class="card"><li><div style="height:17em"><img class="poster-little" src="' + imgurl + '" /></div></li><li><p class="caption">' + title + '<br><span class="movieinfo"><span class="boxed">' + agerating + '</span> &middot; ' + released + ' &middot;&nbsp;' + imdbrating + '</span><br><span class="movieinfo"><span>' + genres + '</span></span></p></li></div></ul></div></div>';
 		
 		smallMovieContainer.insertAdjacentHTML('beforeend', childMovie);
 		switchCount = switchCount + 1;
