e9278ffad8e488213bd78817334345e4984f4b30
Kevin B <marsnebulasoup@gmail.com>
2020-09-11 13:59:04 -0500

    Removed old version

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
----
-permalink: /index.html
----
-not complete. <b>there ARE bugs.</b> <br>
-searches and filters movies depending on what you like. <br>
-currently on lengthapi.win for testing, but will change the domain when project is complete.
-
+### There's nothing here
--- time/script.js
+++
@@ -1,9 +0,0 @@
-function ChangeTimeZone(){
-	var menu = document.getElementById("selTimezone");
-	menu.setAttribute("class", "isActive");
-}
-
-function CloseSelectTimezone(){
-	var menu = document.getElementById("selTimezone");
-	menu.removeAttribute("class", "isActive");
-}
